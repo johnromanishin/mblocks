11378a3afa9444483e459339efcfc4ab3051bfce
johnromanishin <johnrom@mit.edu>
2017-07-31 03:17:35 -0400

    Added some sensor reading and basic functionality

--- mblocks_master/initialization.ino
+++ mblocks_master/initialization.ino
@@ -1,4 +1,46 @@
-void initializeCube(){
-  
+void initializeCube()
+{
+if(get_vin() > 3400) {turn_off_esp();} // This turns off ESP if we are on a charging pad 
 }
 
+
+
+
+void turn_off_esp()
+{
+for(int i = 0; i < 2;i++){Serial.println("fbsleep");delay(300);}// Tell the slaveboard NRF51422 to sleep the face-boards 3 seperate times.
+for(int i = 0; i < 4;i++){Serial.println("espoff");delay(300);}// This should turn the lights completely off... Suicide basically
+}
+
+
+int get_vin()
+// Obtains the input voltage from the Slave Board - this is used to detect when the module is charging, and to put itself to sleep
+{
+  int timeout_time = 60; // ms - how long the function will attempt to parse text for.
+  long begin_function = millis();
+  char prev_char      = ' ';
+  String temp_string  = "";
+  int temp_vin        = 0;
+  while(Serial.available()){Serial.read();} // empty serial buffer just in case...
+  delay(2);
+  Serial.println("vin");
+  delay(23);                                // We need to wait for the slave board to get its shit together
+  while (Serial.available() > 0 && (millis()-begin_function) < timeout_time) // while there are things in the serial buffer...
+  {
+        char c = Serial.read();
+        if(c == ' ' && prev_char == ':')
+          {
+              for(int i = 0; i < 4; i++)
+                  {
+                  char a = Serial.read();
+                  delayMicroseconds(250);
+                  if(isDigit(a)){temp_string += a;}
+                  } 
+              temp_vin = temp_string.toInt();
+        
+          }
+        delayMicroseconds(250);
+        prev_char = c;
+  }
+  return(temp_vin);
+}
--- mblocks_master/sensation.ino
+++ mblocks_master/sensation.ino
@@ -1,1 +1,62 @@
+int read_5048_angle(int address)
+// Returns the angle of the measured magnet as a 14 bit number
+{
+  Wire.beginTransmission(address);
+  Wire.write(byte(255));
+  Wire.endTransmission();
+  Wire.requestFrom(address, 2);
+
+  int reading = 0;
+  if (2 <= Wire.available())
+  {
+    reading = Wire.read();
+    reading = reading << 6;
+    reading |= Wire.read();
+  }
+  return reading;
+}
+
+void read_gyro_and_accel(int address)
+{
+  int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; 
+  wake_accel(address);
+  Wire.beginTransmission(address);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(address,14,1);
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+}
+
+int read_5048_agc(int address)
+// AGC is the "strength" of the magnet returned as an 8-bit number, 255 = magnet field is too weak, 0 = very strong magnetic field.
+{
+  Wire.beginTransmission(address);
+  Wire.write(0xFA);
+  Wire.endTransmission();
+  Wire.requestFrom(address, 2);
+
+  int reading = 0;
+  if (2 <= Wire.available())
+  {
+    reading = Wire.read();
+    reading = reading << 6;
+    reading |= Wire.read();
+  }
+  return reading;
+}
+
+void wake_accel(int address)
+// Wakes the MPU_6050 accelerometer
+{
+  Wire.beginTransmission(address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+} 
 
