8b93a0270e2d0e9070a860d940fe16318d5afab6
Arya Azma <aambp@dhcp-18-111-126-160.dyn.MIT.EDU>
2018-04-03 05:34:48 -0400

    added semicolon

--- serverBlock/Communication.h
+++ serverBlock/Communication.h
@@ -16,7 +16,7 @@ extern painlessMesh mesh;
 extern CircularBuffer<String, true> jsonCircularBuffer;
 
 bool sendMessage(int recipientID, String msg);
-void updateBoxes(CircularBuffer<inboxLog>& inbox, CircularBuffer<outboxLog>& outbox[])
+void updateBoxes(CircularBuffer<inboxLog>& inbox, CircularBuffer<outboxLog>& outbox[]);
 
 void initializeWifiMesh();
 bool sendBroadcastMessage(String message);
